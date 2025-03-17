4b001f313ac81683bfe3deb570a120344181c55a
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-03-17 15:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+        let count = 0;
+        const counterElement = document.getElementById("counter");
+        const button = document.getElementById("incrementBtn");
+
+        button.addEventListener("click", function() {
+            alert(count);
+            count++;
+            counterElement.textContent = count;
+        });
+
